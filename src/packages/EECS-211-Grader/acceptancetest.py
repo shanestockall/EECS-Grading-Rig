9023966675f056d34606c7af904cdde9db704e06
shanestockall2017 <shanestockall2017@u.northwestern.edu>
2017-09-26 16:39:43 -0500

    acceptance test updates

--- src/packages/EECS-211-Grader/acceptancetest.py
+++ src/packages/EECS-211-Grader/acceptancetest.py
@@ -1,17 +1,10 @@
-import time
-import submissionsource
-import submissionunpacker
-import builder
-import tester
-import reporter
-import gradebook
 import hashlib
 import shutil
-from zipfile import zipfile
+import os
 from datetime import datetime
 
 list_of_paths_and_strings = [
-["assignment1.cpp", ["main()"]]
+["assignment1.cpp", "main()"]
 ]
 
 def main():
@@ -37,49 +30,67 @@ def get_md5_hash(file):
 	return file, md5_hash_output
 
 def get_current_time():
-	return datetime.now().time()
+	print "The current time is " + " datetime.today()"
+	return datetime.today()
 
 def acceptance_test():
 	# for each list of the list of paths and strings
 	# make sure that a file with that name exists within the folder 
 	for my_list in list_of_paths_and_strings:
-		path = my_list[0] 
+		path = my_list[0]
 		list_of_strings = my_list[1:]
 		try: 
 			with open(path) as file:
 				for string in list_of_strings:
 					if string in file.read():
-						print "Found " + string + "in file."
+						print "Found " + string +  " in file."
 					else: 
 						print string + "not found in file."
 						return False
 			file.close()
 			return True
-		except: 
-			print path + ': File does not exist. Please make sure all necessary files are in the correct place.'
-
-
+		except:
+			print 'File does not exist. Please make sure all necessary files are in the correct place.'
 			return False
 
 def make_txt_file():
 	# writes a text file with each of the hashes for each of the files using MD5
 	write_file = open("hash.txt", "w+")
-	write_file.write("Write time: " + get_current_time() + '\n')
+	write_file.write("Write time: " + str(get_current_time()) + '\n')
 
 	for file in os.listdir(os.getcwd()):
-		f_name, file_hash = get_md5_hash(file)
-		write_file.write(f_name + '\n')
-		write_file.write(file_hash + '\n')
+                if ".git" in file or ".xcodeproj" in file:
+                        continue
+		elif "." in file:
+			f_name, file_hash = get_md5_hash(file)
+			write_file.write(f_name + '\n')
+			write_file.write(file_hash + '\n')
 
 	write_file.close()	
 
 def zip_dir():
 	# zips directory using shutil.make_archive()
 	zip_name = "submission"
-	directory_name = "."
+	directory_name = "./tmp"
+
+	os.mkdir("./tmp")
+
+	for file in os.listdir(os.getcwd()):
+		try:
+			if ".pdf" in file:
+				continue
+			elif "acceptancetest" in file:
+				continue
+			elif ".git" in file:
+                                continue
+			else:
+				shutil.copy(file, './tmp/')
+		except: 
+			continue
 
 	shutil.make_archive(zip_name, 'zip', directory_name)
 
+	shutil.rmtree('./tmp')
 
 
 
